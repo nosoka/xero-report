582807c20e828104554a605e48f9e96cdaaff740
Asok <asok@fastmail.com>
2019-05-08 08:38:34 +0530

    Initial checkin. barebone laravel package.

---
+++ src/XeroReportServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Nosok\XeroReport;
+
+use Illuminate\Support\ServiceProvider;
+
+class XeroReportServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the package.
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
